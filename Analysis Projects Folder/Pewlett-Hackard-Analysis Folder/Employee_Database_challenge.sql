66759f2e3e856fcadd96b5ffe22e2af611057ae3
unknown <manuel.andrei.lopez@gmail.com>
2022-02-20 23:34:04 -0600

    final

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
@@ -35,3 +35,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+--Deliverable 2
+SELECT DISTINCT ON (emp_no) employees.emp_no, 
+	employees.first_name, 
+employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no=dept_emp.emp_no)
+INNER JOIN titles
+ON (employees.emp_no=titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no;
